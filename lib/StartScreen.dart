919ca828f4af4dc3f51713c9ef51ba4e2feb4329
Jose A. Pinell <japinell@yahoo.com>
2025-07-06 21:08:32 -0700

    Added visual elements to the start screen

--- lib/StartScreen.dart
+++ lib/StartScreen.dart
@@ -5,6 +5,24 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Quiz App"));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/images/quiz-logo.png", width: 300, height: 300),
+          SizedBox(height: 50),
+          Text(
+            "Flutter Quiz App",
+            style: TextStyle(color: Colors.white, fontSize: 32),
+          ),
+          SizedBox(height: 50),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: Text("Start Quiz"),
+          ),
+        ],
+      ),
+    );
   }
 }
