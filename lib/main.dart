8bb9c8106e5c6e3c51f477dabc85271ef1acf4dc
Jose A. Pinell <japinell@yahoo.com>
2025-07-07 18:09:28 -0700

    Added Quiz, and Questions screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:flutter_quiz_app/start_screen.dart";
+import "package:flutter_quiz_app/quiz.dart";
 
 void main() => runApp(const QuizApp());
 
@@ -8,22 +8,6 @@ class QuizApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 154, 117, 196),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    );
+    return (Quiz());
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+
+import "package:flutter_quiz_app/start_screen.dart";
+import "package:flutter_quiz_app/questions_screen.dart"
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen();
+    })
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 154, 117, 196),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
