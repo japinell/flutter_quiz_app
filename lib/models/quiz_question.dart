d2fb8ac61c121c2c92daedcb8c5f119b910bd9cc
Jose A. Pinell <japinell@yahoo.com>
2025-07-11 20:51:33 -0700

    Shuffle the answers before displaying

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,13 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    // Create a copy of the answers list
+    final shuffledList = List.of(answers);
+    // Shuffle the copied list
+    shuffledList.shuffle();
+    // Return the shuffled list
+    return shuffledList;
+  }
 }
