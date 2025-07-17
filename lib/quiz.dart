b8c59ef42bd0bae5ffa65691a4372a2e6df02b78
Jose A. Pinell <japinell@yahoo.com>
2025-07-16 19:38:14 -0700

    Reset answers after last question

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 
 import "package:flutter_quiz_app/start_screen.dart";
 import "package:flutter_quiz_app/questions_screen.dart";
+import "package:flutter_quiz_app/data/questions.dart";
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -24,6 +25,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = "start-screen";
+        selectedAnswers.clear();
+      });
+    }
   }
 
   @override
