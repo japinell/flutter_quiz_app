0f88734db279d1bcd22b4f65564868f678258bb9
Jose A. Pinell <japinell@yahoo.com>
2025-07-18 19:35:26 -0700

    Added summary data

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,8 +1,24 @@
 import "package:flutter/material.dart";
+import "package:flutter_quiz_app/data/questions.dart";
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.selectedAnswers});
   final List<String> selectedAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i <= selectedAnswers.length; i++) {
+      summary.add({
+        "index": i,
+        "question": questions[i].text,
+        "correct_answer": questions[i].answers[0],
+        "user_answer": selectedAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
